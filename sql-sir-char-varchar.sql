211ae6a23c07d33395a425cfcc00fec75bfdd1d4
MamtaJangid <mamtajangid2511@gmail.com>
2024-08-08 22:07:35 +0530

    Add files via upload
    sir char var query

---
+++ sql-sir-char-varchar.sql
@@ -0,0 +1,27 @@
+ use regex1234;
+ use sakila;
+ create database regex1234;
+ show databases;
+ create database Mamta;
+ show databases;
+ use Mamta;
+  -- char var char 
+ create table e_var(name1 varchar(4));
+ insert  into e_var values("Xf   ");
+ select * from name1,length(name1) from e_var;
+ 
+create table student123(dob date);
+insert into student123 values('2024-12-10');
+select * from student123
+
+create table student1234(dob datetime);
+insert into student1234 values('2024-12-10');
+select * from student1234;
+
+-- q timestamp datetime interview question 
+
+use sakila;
+create table priduct(pid int ,pname varchar(20));
+insert into product (pid,pname)
+values (10,"mobie"),(11,"apple"),(12,"tv");
+select * from product;
